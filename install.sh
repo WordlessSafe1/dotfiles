31b756eb65ccbe82a2f80e31aacdf5e2b5d7ac69
Connor Cummings <94203733+WordlessSafe1@users.noreply.github.com>
2025-02-27 16:50:56 -0600

    Update install.sh to grab files dynamically

--- install.sh
+++ install.sh
@@ -13,9 +13,21 @@ if [[ $1 == "link" ]]; then
 		ln -s ~/.dotfiles/$file ~/$file
 	done
 else
-	git clone https://github.com/WordlessSafe1/dotfiles .dotfiles
+	mkdir -p ~/.dotfiles
+	cd ~/.dotfiles
 
-	cd .dotfiles
+	files=$(curl https://api.github.com/repos/WordlessSafe1/dotfiles/contents/)
+	files=($(grep -oE "\"path\":\s*\"([^\"]+)\"" <<< "$files"))
+	
+	for file in "${files[@]}" ; do
+		if [[ $file == "\"path\":" ]]; then
+			continue
+		fi
+		file="${file%?}"
+		file="${file:1}"
+		wget https://raw.githubusercontent.com/WordlessSafe1/dotfiles/refs/heads/master/$file
+	done
+
 	./install.sh link
 fi
 
